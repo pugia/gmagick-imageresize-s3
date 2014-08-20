a26ee8ed72dbf3ed45528d5fb94dcc9af133efec
Marco Pugliese <marco.pugliese@pugia.com>
2014-08-19 20:03:27 -0700

    Track new relic

--- private/config.inc.php
+++ private/config.inc.php
@@ -4,3 +4,5 @@ include dirname(__FILE__).'/../vendor/autoload.php';
 include dirname(__FILE__).'/s3.inc.php';
 
 $ext_myme = array('jpg' => 'image/jpeg', 'jpeg' => 'image/jpeg', 'png' => 'image/png');
+
+if (extension_loaded('newrelic')) { newrelic_set_appname('Image Resize'); }
